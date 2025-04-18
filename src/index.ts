1dc08b085781304f5fd80837171281c31f8968e4
Ann Catherine Jose <ann.jose@gmail.com>
2025-04-18 09:27:03 -0700

    Integrated Durables objects into this project to work with Workflows. Deployed and working fine.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,7 @@
 // <docs-tag name="full-workflow-example">
 import { WorkflowEntrypoint, WorkflowStep, WorkflowEvent } from 'cloudflare:workers';
+import { DurableObject } from "cloudflare:workers";
 
-type Env = {
-	// Add your bindings here, e.g. Workers KV, D1, Workers AI, etc.
-	MY_WORKFLOW: Workflow;
-};
 
 // User-defined params passed to your workflow
 type Params = {
@@ -12,12 +9,50 @@ type Params = {
 	metadata: Record<string, string>;
 };
 
+export class MyDurableObject extends DurableObject<Env> {
+	/**
+	 * The constructor is invoked once upon creation of the Durable Object, i.e. the first call to
+	 * 	`DurableObjectStub::get` for a given identifier (no-op constructors can be omitted)
+	 *
+	 * @param ctx - The interface for interacting with Durable Object state
+	 * @param env - The interface to reference bindings declared in wrangler.jsonc
+	 */
+	constructor(ctx: DurableObjectState, env: Env) {
+		super(ctx, env);
+	}
+
+	/**
+	 * The Durable Object exposes an RPC method sayHello which will be invoked when a Durable
+	 *  Object instance receives a request from a Worker via the same method invocation on the stub
+	 *
+	 * @param name - The name provided to a Durable Object instance from a Worker
+	 * @returns The greeting to be sent back to the Worker
+	 */
+	async sayHello(name: string): Promise<string> {
+		return `Hello, ${name}!`;
+	}
+}
+
+
 // <docs-tag name="workflow-entrypoint">
 export class MyWorkflow extends WorkflowEntrypoint<Env, Params> {
 
 	async run(event: WorkflowEvent<Params>, step: WorkflowStep) {
 		// Can access bindings on `this.env`
 		// Can access params on `event.payload`
+
+		// const xyz = await step.do('Step 0: Read Durable Object MY_DURABLE_OBJECT', async () => {
+		// 	// Read from a Durable Object
+		// 	const id: DurableObjectId = this.env.MY_DURABLE_OBJECT.idFromName("foo");
+
+		// 	// Create a stub to open a communication channel with the Durable
+		// 	// Object instance.
+		// 	const stub = this.env.MY_DURABLE_OBJECT.get(id);
+
+		// 	// Call the `sayHello()` RPC method on the stub to invoke the method on
+		// 	// the remote Durable Object instance
+		// 	const greeting = await stub.sayHello("world");
+		// });
 
 		const files = await step.do('Step 1: Fetch some files', async () => {
 			// Fetch a list of files from $SOME_SERVICE
@@ -49,26 +84,26 @@ export class MyWorkflow extends WorkflowEntrypoint<Env, Params> {
 			return await resp.json<any>();
 		});
 
-		await step.sleep('Sleep: wait on something', '1 minute');
+		// await step.sleep('Sleep: wait on something', '1 minute');
 
-		await step.do(
-			'Step 4: Make a call to write to DB - and it may fail',
-			// Define a retry strategy
-			{
-				retries: {
-					limit: 5,
-					delay: '5 second',
-					backoff: 'exponential',
-				},
-				timeout: '15 minutes',
-			},
-			async () => {
-				// Do stuff here, with access to the state from our previous steps
-				if (Math.random() > 0.5) {
-					throw new Error('API call to $STORAGE_SYSTEM failed');
-				}
-			},
-		);
+		// await step.do(
+		// 	'Step 4: Make a call to write to DB - and it may fail',
+		// 	// Define a retry strategy
+		// 	{
+		// 		retries: {
+		// 			limit: 5,
+		// 			delay: '5 second',
+		// 			backoff: 'exponential',
+		// 		},
+		// 		timeout: '15 minutes',
+		// 	},
+		// 	async () => {
+		// 		// Do stuff here, with access to the state from our previous steps
+		// 		if (Math.random() > 0.5) {
+		// 			throw new Error('API call to $STORAGE_SYSTEM failed');
+		// 		}
+		// 	},
+		// );
 	}
 }
 // </docs-tag name="workflow-entrypoint">
@@ -92,6 +127,18 @@ export default {
 			});
 		}
 
+			// Read from a Durable Object
+			const id_new: DurableObjectId = env.MY_DURABLE_OBJECT.idFromName("foo");
+
+			// Create a stub to open a communication channel with the Durable
+			// Object instance.
+			const stub = env.MY_DURABLE_OBJECT.get(id_new);
+
+			// Call the `sayHello()` RPC method on the stub to invoke the method on
+			// the remote Durable Object instance
+			const greeting = await stub.sayHello("world new");
+
+
 		// Spawn a new instance and return the ID and status
 		let instance = await env.MY_WORKFLOW.create();
 		// You can also set the ID to match an ID in your own system
@@ -103,6 +150,7 @@ export default {
 		return Response.json({
 			id: instance.id,
 			details: await instance.status(),
+			greeting
 		});
 	},
 };
